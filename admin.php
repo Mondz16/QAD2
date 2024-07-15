2c4598adf3d6ff66e30922f43b638728a1661e5d
Christian Mundal <70058199+Mondz16@users.noreply.github.com>
2024-07-15 11:59:37 +0800

    Updated Admin Dashboard

--- admin.php
+++ admin.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 ?>
-<!-- // test change 1 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,23 +10,49 @@ session_start();
     <link rel="stylesheet" href="admin_style.css">
 </head>
 <body>
-<header class="site-header">
-    <nav>
-        <ul class="nav-list">
-            <li><a href="college.php">College</a></li>
-            <li><a href="schedule.php">Schedule</a></li>
-            <li><a href="orientation.php">Orientation</a></li>
-            <li><a href="assessment.php">Assessment</a></li>
-            <li><a href="udas_assessment.php">UDAS Assessment</a></li>
-            <li><a href="report.php">Report</a></li>
-            <li><a href="registration.php">Registration</a></li>
-            <li class="btn"><a href="logout.php">Log Out</a></li>
-        </ul>
-    </nav>
-</header>
-<div class="admin-content">
-    <h1>Welcome to the Admin Panel</h1>
-    <p>Select an option from the navigation bar to manage different sections.</p>
-</div>
+    <div class="wrapper">
+        <div class="hair" style="height: 15px; background: linear-gradient(275.52deg, #973939 0.28%, #DC7171 100%);"></div>
+
+        <div class="container">
+            <div class="header">
+                 <div class="headerLeft">
+                    <div class=USePData>
+                        <img class="USeP" src="images/USePLogo.png" height="36">
+                        <div style="height: 0px; width: 16px;"></div>
+                        <div style="height: 32px; width: 1px; background: #E5E5E5"></div>
+                        <div style="height: 0px; width: 16px;"></div>
+                        <div class="headerLeftText">
+                            <div class="onedata" style="height: 100%; width: 100%; display: flex; flex-flow: unset; place-content: unset; align-items: unset; overflow: unset;">
+                                <h><span class="one" style="color: rgb(229, 156, 36); font-weight: 600; font-size: 18px;">One</span>
+                                    <span class="datausep" style="color: rgb(151, 57, 57); font-weight: 600; font-size: 18px;">Data.</span>
+                                    <span class="one" style="color: rgb(229, 156, 36); font-weight: 600; font-size: 18px;">One</span>
+                                    <span class="datausep" style="color: rgb(151, 57, 57); font-weight: 600; font-size: 18px;">USeP.</span></h>
+                            </div>
+                                <h>Accreditor Portal</h>
+                        </div>
+                    </div>
+                </div>
+                <div class="headerRight">
+                    <a class="btn" href="logout.php">Log Out</a>
+                </div>
+            </div>
+        </div>
+        <div style="height: 1px; width: 100%; background: #E5E5E5"></div>
+        <div class="admin-content">
+            <h1>Welcome to the Admin Panel</h1>
+            <p>Select an option from the navigation bar to manage different sections.</p>
+        </div>
+        <nav>
+            <ul class="nav-list">
+                <li><a href="college.php">College</a></li>
+                <li><a href="schedule.php">Schedule</a></li>
+                <li><a href="orientation.php">Orientation</a></li>
+                <li><a href="assessment.php">Assessment</a></li>
+                <li><a href="udas_assessment.php">UDAS Assessment</a></li>
+                <li><a href="report.php">Report</a></li>
+                <li><a href="registration.php">Registration</a></li>
+            </ul>
+        </nav>
+    </div>
 </body>
 </html>
