d8f6882952da19c0f24e0d455f3c847b39bc524c
Christian Mundal <70058199+Mondz16@users.noreply.github.com>
2024-07-22 14:55:47 +0800

    Fixed Login

--- login.php
+++ login.php
@@ -1,41 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/pagestyle.css" rel="stylesheet">
-=======
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    <link href="css/pagestyle.css" rel="stylesheet">
->>>>>>> Stashed changes
-=======
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    <link href="css/pagestyle.css" rel="stylesheet">
->>>>>>> Stashed changes
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="css/pagestyle.css" rel="stylesheet">
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row top-bar"></div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        <div class="row header">
-            <div class="col-12 col-md-6 d-flex align-items-center">
-                <img src="images/USePLogo.png" alt="USeP Logo">
-                <div class="divider"></div>
-=======
-=======
->>>>>>> Stashed changes
         <div class="row header mb-3">
             <div class="col-6 col-md-2 mx-auto d-flex align-items-center justify-content-end">
                 <img src="images/USePLogo.png" alt="USeP Logo">
             </div>
             <div class="col-6 col-md-4 d-flex align-items-center">
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                 <div class="vertical-line"></div>
                 <div class="divider"></div>
                 <div class="text">
@@ -46,8 +29,6 @@
                     <span>Accreditor Portal</span>
                 </div>
             </div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             <div class="col-6 d-none d-md-flex align-items-center justify-content-end">
                 <div class="text-right"></div>
                 <span>Quality Assurance Division</span>
@@ -58,38 +39,9 @@
             </div>
         </div>
 
-        <div class="row justify-content-left content">
-            <div style="height: 0px; width: 45px;"></div>
-            <div class="col-md-6 col-lg-4">
-=======
-            <div class="col-md-4 d-none d-md-flex align-items-center justify-content-end">
-                <span>Quality Assurance Division</span>
-                <div class="divider"></div>
-                <div class="vertical-line"></div>
-            </div>
-            <div class="col-md-2 d-none d-md-flex align-items-center justify-content-start">
-                <img src="images/sdmdlogo.png" alt="USeP Logo">
-            </div>
-        </div>
-=======
-            <div class="col-md-4 d-none d-md-flex align-items-center justify-content-end">
-                <span>Quality Assurance Division</span>
-                <div class="divider"></div>
-                <div class="vertical-line"></div>
-            </div>
-            <div class="col-md-2 d-none d-md-flex align-items-center justify-content-start">
-                <img src="images/sdmdlogo.png" alt="USeP Logo">
-            </div>
-        </div>
->>>>>>> Stashed changes
-        
         <div class="row justify-content-start">
             <div class="col-md-2"></div>
             <div class="col-md-6 col-lg-4 mt-5">
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                 <h1>Welcome Back!</h1>
                 <p>Please login to get started.</p>
                 <form action="login_process.php" method="post">
@@ -102,49 +54,22 @@
                         </div>
                         <a href="#">Forgot password?</a>
                     </div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-                    <div class="row mt-3">
-                        <div class="col-6">
-                            <a href="register.php" class="btn btn-block" style="color: black; border: 1px solid; border-radius: 10px; outline: unset; text-decoration: none; cursor: pointer; padding: 16px; font-size: 16px;">SIGN UP</a>
-                        </div>
-                        <div class="col-6">
-                            <button type="submit" value="Login" class="btn btn-block" style="color: white; background: linear-gradient(275.52deg, #FF7A7A 0.28%, #E6A33E 100%); border: unset; outline: unset; text-decoration: none; cursor: pointer; border-radius: 10px; padding: 16px; font-size: 16px;">LOG IN</button>
-=======
-=======
->>>>>>> Stashed changes
                     <div class="container mt-3">
                         <div class="row mt-3 justify-content-start">
                             <button type="submit" value="Login" class="pobtn btn btn-block me-md-2 col-md-4 col-12 mb-3 mb-md-0">LOG IN</button>
                             <a href="register.php" class="nebtn btn btn-block col-md-4 col-12">SIGN UP</a>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-        <footer class="text-center mt-5">
-            <p>Copyright © 2024. All Rights Reserved.</p>
-            <a href="#">Terms of Use</a> | <a href="#">Privacy Policy</a>
-=======
-=======
->>>>>>> Stashed changes
         <footer class="row text-left mt-5">
-        <div class="col-2"></div>
-        <div class="col">
-            <p>Copyright © 2024. All Rights Reserved.</p>
-            <a href="#">Terms of Use</a> | <a href="#">Privacy Policy</a>
-        </div>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+            <div class="col-2"></div>
+            <div class="col">
+                <p>Copyright © 2024. All Rights Reserved.</p>
+                <a href="#">Terms of Use</a> | <a href="#">Privacy Policy</a>
+            </div>
         </footer>
     </div>
 
@@ -160,14 +85,7 @@
     </script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     <script src="js/bootstrap.min.js"></script>
 </body>
-=======
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script></body>
->>>>>>> Stashed changes
-=======
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script></body>
->>>>>>> Stashed changes
+
 </html>
