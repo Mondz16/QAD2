9f10cebffc4e1d388da330523d05e123b5619c04
beanss26 <viencejhonalimento.banzali@gmail.com>
2024-08-20 17:01:45 +0800

    add college

--- add_college_process.php
+++ add_college_process.php
@@ -4,26 +4,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OPERATION RESULT</title>
+    <title>Operation Result</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap">
-        <link rel="stylesheet" href="index.css">
-        <style>
+    <style>
         * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
             font-family: "Quicksand", sans-serif;
         }
+
         body {
-            background-color: rgba(0, 0, 0, 0.5);
+            background-color: #f9f9f9;
             display: flex;
             align-items: center;
             justify-content: center;
             height: 100vh;
         }
+
+        .container {
+            max-width: 750px;
+            padding: 24px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
         h2 {
             font-size: 24px;
-            color: #292D32;
+            color: #973939;
             margin-bottom: 20px;
         }
 
@@ -31,6 +41,7 @@
             margin-bottom: 20px;
             font-size: 18px;
         }
+
         .success {
             color: green;
         }
@@ -38,22 +49,29 @@
         .error {
             color: red;
         }
-        .btn-hover{
-            border: 1px solid #AFAFAF;
-            text-decoration: none;
-            color: black;
-            border-radius: 10px;
-            padding: 20px 50px;
-            font-size: 1rem;
-            font-weight: bold;
-            text-transform: uppercase;
+
+        .button-primary {
+            background-color: #2cb84f;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+            border-radius: 4px;
+            margin-top: 10px;
+            color: white;
+            font-size: 16px;
         }
-        .btn-hover:hover {
-            background-color: #AFAFAF;
+
+        .button-primary:hover {
+            background-color: #259b42;
         }
     </style>
 </head>
+
 <body>
+    <div class="container">
+        <h2>Operation Result</h2>
+        <div class="message">
             <?php
             $servername = "localhost";
             $username = "root";
@@ -63,17 +81,7 @@
             $conn = new mysqli($servername, $username, $password, $dbname);
 
             if ($conn->connect_error) {
-                die("
-                <div class='popup-content'>
-                    <div style='height: 50px; width: 0px;'></div>
-                    <img class='Error' src='images/Error.png' height='100'>
-                    <div style='height: 25px; width: 0px;'></div>
-                    <p class='mb-4 mt-4  text-muted error'>Connection failed: " . $conn->connect_error . "</p>
-                    <div style='height: 50px; width: 0px;'></div>
-                    <a href='college.php'class='btn-hover'>OKAY</a>
-                    <div style='height: 100px; width: 0px;'></div>
-                    <div class='hairpop-up'></div>
-                </div>");
+                die("<p class='error'>Connection failed: " . $conn->connect_error . "</p>");
             }
 
             $college_name = $_POST['college_name'];
@@ -107,17 +115,7 @@
                 }
 
                 if (intval($next_code) > 15) {
-                    die("
-                    <div class='popup-content'>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <img class='Error' src='images/Error.png' height='100'>
-                        <div style='height: 25px; width: 0px;'></div>
-                        <p class='mb-4 mt-4  text-muted error'>All codes from 01 to 15 have been used for $table.</p>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <a href='college.php'class='btn-hover'>OKAY</a>
-                        <div style='height: 100px; width: 0px;'></div>
-                        <div class='hairpop-up'></div>
-                    </div>");
+                    die("<p class='error'>All codes from 01 to 15 have been used for $table.</p>");
                 }
 
                 return $next_code;
@@ -136,17 +134,7 @@
                 // College name exists, use its college_code
                 $row_check = $result_check->fetch_assoc();
                 $college_code = $row_check['code'];
-                echo "
-                <div class='popup-content'>
-                    <div style='height: 50px; width: 0px;'></div>
-                    <img class='Success' src='images/Success.png' height='100'>
-                    <div style='height: 20px; width: 0px;'></div>
-                    <p class='mb-4 mt-4  text-muted success'>College already exists.<br>Program(s) added successfully.</p>
-                    <div style='height: 50px; width: 0px;'></div>
-                    <a href='college.php'class='btn-hover rounded-3 px-5 py-3'>OKAY</a>
-                    <div style='height: 100px; width: 0px;'></div>
-                    <div class='hairpop-up'></div>
-                </div>";
+                echo "<p class='success'>College already exists. Program(s) added successfully.</p>";
             } else {
                 // College name does not exist, insert it into the table
                 $college_code = getNextCode('college', $conn);
@@ -156,29 +144,9 @@
                 $stmt_insert_college->bind_param("ssss", $college_code, $college_name, $college_campus, $college_email);
 
                 if ($stmt_insert_college->execute()) {
-                    echo "
-                    <div class='popup-content'>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <img class='Success' src='images/Success.png' height='100'>
-                        <div style='height: 20px; width: 0px;'></div>
-                        <p class='mb-4 mt-4  text-muted success'>College added successfully.</p>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <a href='college.php'class='btn-hover'>OKAY</a>
-                        <div style='height: 100px; width: 0px;'></div>
-                        <div class='hairpop-up'></div>
-                    </div>";
+                    echo "<p class='success'>College added successfully.</p>";
                 } else {
-                    echo "
-                    <div class='popup-content'>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <img class='Error' src='images/Error.png' height='100'>
-                        <div style='height: 25px; width: 0px;'></div>
-                        <p class='mb-4 mt-4  text-muted error'>Error adding college: " . $conn->error . "</p>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <a href='college.php'class='btn-hover'>OKAY</a>
-                        <div style='height: 100px; width: 0px;'></div>
-                        <div class='hairpop-up'></div>
-                    </div>";
+                    echo "<p class='error'>Error adding college: " . $conn->error . "</p>";
                     $stmt_insert_college->close();
                     $conn->close();
                     exit;
@@ -261,17 +229,7 @@
                         $stmt_program_level_history->bind_param("isss", $program_id, $level, $date_received, $year_of_validity);
 
                         if (!$stmt_program_level_history->execute()) {
-                            echo "
-                                <div class='popup-content'>
-                                    <div style='height: 50px; width: 0px;'></div>
-                                    <img class='Error' src='images/Error.png' height='100'>
-                                    <div style='height: 25px; width: 0px;'></div>
-                                    <p mb-4 mt-4  text-muted class='error'>Error adding program level history for '$program': " . $conn->error . "</p>
-                                    <div style='height: 50px; width: 0px;'></div>
-                                    <a href='college.php'class='btn-hover'>OKAY</a>
-                                    <div style='height: 100px; width: 0px;'></div>
-                                    <div class='hairpop-up'></div>
-                                </div>";
+                            echo "<p class='error'>Error adding program level history for '$program': " . $conn->error . "</p>";
                             $program_success = false;
                             continue; // Skip to the next program if insertion fails
                         }
@@ -285,34 +243,16 @@
                         $stmt_update_program->bind_param("ii", $program_level_id, $program_id);
 
                         if (!$stmt_update_program->execute()) {
-                            echo "
-                                <div class='popup-content'>
-                                    <div style='height: 50px; width: 0px;'></div>
-                                    <img class='Error' src='images/Error.png' height='100'>
-                                    <div style='height: 25px; width: 0px;'></div>
-                                    <p class='mb-4 mt-4  text-muted error'>Error updating program with level ID for '$program': " . $conn->error . "</p>
-                                    <div style='height: 50px; width: 0px;'></div>
-                                    <a href='college.php'class='btn-hover'>OKAY</a>
-                                    <div style='height: 100px; width: 0px;'></div>
-                                    <div class='hairpop-up'></div>
-                                </div>";
+                            echo "<p class='error'>Error updating program with level ID for '$program': " . $conn->error . "</p>";
                             $program_success = false;
                         }
                     }
                 }
 
                 if (!$program_success) {
-                    echo "
-                    <div class='popup-content'>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <img class='Error' src='images/Error.png' height='100'>
-                        <div style='height: 25px; width: 0px;'></div>
-                        <p class='mb-4 mt-4  text-muted error'>Some programs could not be added.</p>
-                        <div style='height: 50px; width: 0px;'></div>
-                        <a href='college.php'class='btn-hover'>OKAY</a>
-                        <div style='height: 100px; width: 0px;'></div>
-                        <div class='hairpop-up'></div>
-                    </div>";
+                    echo "<p class='error'>Some programs could not be added or updated.</p>";
+                } else {
+                    echo "<p class='success'>All programs processed successfully.</p>";
                 }
 
                 $stmt_program->close();
@@ -321,7 +261,8 @@
             $conn->close();
             ?>
         </div>
-        </div>
-    </body>
+        <button class="button-primary" onclick="window.location.href='college.php'">OK</button>
+    </div>
+</body>
 
 </html>
