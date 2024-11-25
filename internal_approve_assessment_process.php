a5f6ca9bf6eba5a7f3d92cbeadc1c6314e13e1b7
0Lit0 <dracosdraghell@gmail.com>
2024-11-25 16:10:58 +0800

    Update internal_approve_assessment_process.php

--- internal_approve_assessment_process.php
+++ internal_approve_assessment_process.php
@@ -144,7 +144,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         $mail->Host = 'smtp.gmail.com';
                         $mail->SMTPAuth = true;
                         $mail->Username = 'usepqad@gmail.com';
-                        $mail->Password = 'vmvf vnvq ileu tmev';
+                        $mail->Password = 'ofcx jwfa ghkv hsgz';
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                         $mail->Port = 587;
 
